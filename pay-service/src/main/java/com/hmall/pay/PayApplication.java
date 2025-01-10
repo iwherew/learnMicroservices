ed2f2075d10576f9a600a5266a00ca9d3635d46d
wanglilong <wanglilong@croot.com>
2025-01-10 16:45:03 +0800

    mq

--- pay-service/src/main/java/com/hmall/pay/PayApplication.java
+++ pay-service/src/main/java/com/hmall/pay/PayApplication.java
@@ -1,13 +1,14 @@
 package com.hmall.pay;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 // 需要调试的时候才开启feign日志
-//@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
-@EnableFeignClients(basePackages = "com.hmall.api.client")
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
+//@EnableFeignClients(basePackages = "com.hmall.api.client")
 @MapperScan("com.hmall.pay.mapper")
 @SpringBootApplication
 public class PayApplication {
